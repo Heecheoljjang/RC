3844604e25c765cffb396a87da061c5f32e933f9
윤희철 <kkll135@naver.com>
2022-02-11 19:49:54 +0900

    끝

---
+++ 1주차/SpotifyUI/SpotifyUI/ParkViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ParkViewController.swift
+//  SpotifyUI
+//
+//  Created by 희철 on 2022/02/11.
+//
+
+import UIKit
+
+class ParkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func clse(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
--- 1주차/SpotifyUI/SpotifyUI/SearchViewController.swift
+++ 1주차/SpotifyUI/SpotifyUI/SearchViewController.swift
@@ -18,17 +18,6 @@ class SearchViewController: UIViewController {
         searchBar.searchTextField.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
 extension UIColor {
     class var custom: UIColor? { return UIColor(named: "Custom")}
